3947648e47f1063af7a4b813d54030c1f87d8522
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-16 01:27:38 -0600

    Created Home component for Home page

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import { DataContext } from '../../contexts/DataContext';
+
+const Home = () => {
+  return <section className="home"></section>;
+};
+
+export default Home;
