6363a585f43d210d1840a736c7d940e0994eea81
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-16 01:26:13 -0600

    added tests folder

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom'; // optional
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
