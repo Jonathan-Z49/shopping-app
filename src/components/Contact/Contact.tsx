762717c70eced88e3969ce68d24e8ed714a19259
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-19 21:17:31 -0600

    Modified Contact component to now display fake contact info

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <main className="contact">
+      <h1 className="contact-header">We&apos;d Love to Talk</h1>
+      <div className="contact-info">
+        <p>Fake Street 555, Austin, TX 78572</p>
+        <p>1-800-SHOPLY</p>
+        <p>info@shoply.net</p>
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
