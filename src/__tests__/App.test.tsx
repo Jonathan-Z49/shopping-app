4ac76705443138fabf693f201194d80055460aad
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-19 21:58:25 -0600

    Cleanup

--- src/__tests__/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'; // optional
-
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import React from 'react';
