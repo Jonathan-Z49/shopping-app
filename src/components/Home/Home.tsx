3c87c4b94e61dc2aa167f5844b5bc1e65bcdf8b5
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-19 21:18:00 -0600

    Updated Home component to display quotes, sliding messages

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Home = () => {
-  return <main className="home"></main>;
+  return (
+    <main className="home">
+      <section className="messages">
+        <h1 className="header-message">LATEST TECH RELEASES!</h1>
+        <div className="message-container">
+          <ul className="shop-message-list">
+            <li>Discover new tech</li>
+            <li>Explore many products</li>
+            <li>Find great deals</li>
+            <li>Only available at Shop-ly</li>
+          </ul>
+        </div>
+      </section>
+      <h1 className="shop-quote">
+        Ranked Top 1 Electronics Store in the World! <br />- Forbes
+      </h1>
+    </main>
+  );
 };
 
 export default Home;
